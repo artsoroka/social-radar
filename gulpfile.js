ef56a006cd33db583ccc52ed7062d7e313dd6b6d
artsoroka <githubrepository@yandex.ru>
2015-10-06 22:53:35 +0300

    added gulp minify task

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,19 @@ var gulp = require('gulp');
 var browserify = require('browserify'); 
 var reactify = require('reactify'); 
 var source = require('vinyl-source-stream'); 
-
+var uglify = require('gulp-uglify')
 gulp.task('browserify', function(){
     browserify('./src/main.js')
         .transform('reactify')
         .bundle()
         .pipe(source('bundle.js'))
         .pipe(gulp.dest('./dist/')); 
+}); 
+
+gulp.task('minify', function(){
+  gulp.src('./public/bundle.js')
+	.pipe(uglify())
+	.pipe(gulp.dest('./public/'));
 }); 
 
 gulp.task('copy', function(){

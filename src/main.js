2fe18668c206ab16b52b8c42634eb7ecf6b1c773
artsoroka <githubrepository@yandex.ru>
2015-10-08 21:00:25 +0300

    removed react script tag, including with browserify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 /*global io*/ 
 /*global React*/ 
+var React    = require('react'); 
 var App      = require('./components/app');  
 
 var settings = JSON.parse(document.getElementById('settings').innerHTML); 
